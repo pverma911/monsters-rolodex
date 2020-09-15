f1798ab1b3db7a2d5e94a8b00cdaf9e145b7b794
pverma911 <verma.pranshu689@gmail.com>
2020-09-15 20:36:02 +0530

    Updates

---
+++ precache-manifest.405ca6e146747eb9d3278267434fbda4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f804b7d6e55d35a72b3990e767bfb9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4faf886f1a92bba8b15c",
+    "url": "/monsters-rolodex/static/css/main.b5ed661c.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4faf886f1a92bba8b15c",
+    "url": "/monsters-rolodex/static/js/main.09d9be16.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
